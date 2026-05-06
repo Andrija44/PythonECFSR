52bd12cfeb4ff0e6f67e62d2f6a4917bfb9a2766
Andrija44 <macekandrija@gmail.com>
2026-05-07 01:24:49 +0200

    Azurirana setup datoteka

--- wrapper/out/setup.py
+++ wrapper/out/setup.py
@@ -1,14 +1,10 @@
-#!/usr/bin/env python
-
+import sys
 from setuptools import setup
+
+ext_name = 'ecf_wrap.pyd' if sys.platform.startswith('win') else 'ecf_wrap.so'
 
 setup(
     name='ecf_wrap',
-    # version='1.6.6',
-    # description='Python GP-GOMEA',
-    # author='Marco Virgolin',
-    # author_email='marco.virgolin@cwi.nl',
-    # url='https://github.com/marcovirgolin/GP-GOMEA',
     packages=['ecf_wrap'],
-    package_data={'ecf_wrap': ['ecf_wrap.so', 'parameters.txt']},
+    package_data={'ecf_wrap': [ext_name, 'parameters.txt']},
 )
